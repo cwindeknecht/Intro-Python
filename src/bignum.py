811727f80db9979c765d65078b366014e851dc0a
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-07-23 09:59:15 -0700

    bignum.py & hello.py complete

--- src/bignum.py
+++ src/bignum.py
@@ -1,1 +1,8 @@
 # Print out 2 to the 65536 power
+
+# bignum = pow(2,1024)
+# math.pow(2,1024) results in an overflow error
+# the maximum representable value is
+# 1 * 2 ^ ( 2 ^ 10 - 1 )
+# which is 2 ^ 1023
+print(2**65536)
