eda903ad7cb1484629837faa450ae4674dad166e
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-07-24 09:55:26 -0700

    Added args.py
    File to showcase *args and **kwargs

---
+++ src/args.py
@@ -0,0 +1,14 @@
+def myfunction(a,b, *args, **kwargs):
+    print(a)
+    print(b)
+    print(args)
+    print(kwargs)
+
+myfunction(1,2,) #args empty tuple, kwargs empty dict
+myfunction(1,2,3,4) #args gathers non-positional arguments, kwargs empty dict
+myfunction(1,2,3,4,x=5) #kwargs is a dict with a key of x and a value of 5
+
+d = {'1':1,'2':2,'3':3}
+myfunction(1,2,3,4,**d, y=5) #dict / y is kwargs
+myfunction(1,2,3,4,d) #dict is part of args
+myfunction(1,2,*d) #passes keys into args array
